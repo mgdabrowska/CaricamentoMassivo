26e024a9bfe631604394c770ca4585cc76d51b15
Stawicka Malgorzata (UniCredit Business Integrated Solutions) <C311681@hd00.UNICREDITGROUP.EU>
2017-11-13 11:39:32 +0100

    .gitignore

--- src/main/java/pl/gosia/CaricamentoMassivoMaven/Main.java
+++ src/main/java/pl/gosia/CaricamentoMassivoMaven/Main.java
@@ -10,13 +10,13 @@ import javafx.scene.layout.AnchorPane;
 
 
 
-public class Main extends Application {
+public  class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = FXMLLoader.load(getClass().getResource("CaricamentoMassivoMaven.fxml"));
+			AnchorPane root = FXMLLoader.load(getClass().getResource("/CaricamentoMassivoMaven.fxml"));
 			Scene scene = new Scene(root,600,600);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
